b1fdcbd541d6ce74e05562b6e0869e440c6e4f9c
Petrov Dumitru <petrov.dumitru@gmail.com>
2019-07-26 20:24:55 +0300

    saving..

--- day20/day20.php
+++ day20/day20.php
@@ -23,109 +23,181 @@ function	display($map)
 	}
 }
 
-function	compile_map($regex)
+function	map_point(&$point, $to, $matches)
+{
+	if ($point == $matches)
+		$point = $to;
+}
+
+function	move_west(&$x, &$y, &$map, $void)
+{
+	$map[$y][--$x] = '|';
+	if (--$x < 0) {
+		$x = 1;
+		foreach ($map as &$row) {
+			array_unshift($row, $void);
+			array_unshift($row, $void);
+		}
+	}
+	map_point($map[$y - 1][$x], '?', $void);
+	map_point($map[$y][$x], '.', $void);
+	map_point($map[$y + 1][$x], '?', $void);
+
+	$map[$y - 1][$x - 1] = '#';
+	map_point($map[$y][$x - 1], '?', $void);
+	$map[$y + 1][$x - 1] = '#';
+}
+
+function	move_north(&$x, &$y, &$map, $void)
+{
+	$map[--$y][$x] = '-';
+	if (--$y < 0) {
+		$y = 1;
+		array_unshift($map, array_fill(0, count($map[$y]), $void));
+		array_unshift($map, array_fill(0, count($map[$y]), $void));
+	}
+	map_point($map[$y][$x - 1], '?', $void);
+	map_point($map[$y][$x], '.', $void);
+	map_point($map[$y][$x + 1], '?', $void);
+
+	$map[$y - 1][$x - 1] = '#';
+	map_point($map[$y - 1][$x], '?', $void);
+	$map[$y - 1][$x + 1] = '#';
+}
+
+function	move_south(&$x, &$y, &$map, $void)
+{
+	$map[++$y][$x] = '-';
+	if (++$y >= count($map)) {
+		$map[] = array_fill(0, count($map[0]), $void);
+		$map[] = array_fill(0, count($map[0]), $void);
+	}
+	map_point($map[$y][$x - 1], "?", $void);
+	map_point($map[$y][$x], '.', $void);
+	map_point($map[$y][$x + 1], '?', $void);
+
+	$map[$y + 1][$x - 1] = '#';
+	map_point($map[$y + 1][$x], '?', $void);
+	$map[$y + 1][$x + 1] = '#';
+}
+
+function	move_east(&$x, &$y, &$map, $void)
+{
+	$map[$y][++$x] = '|';
+	if (++$x >= count($map[$y])) {
+		for ($i = 0; $i < count($map); $i++) {
+			$map[$i][] = $void;
+			$map[$i][] = $void;
+		}
+	}
+	map_point($map[$y - 1][$x], '?', $void);
+	map_point($map[$y][$x], '.', $void);
+	map_point($map[$y + 1][$x], '?', $void);
+
+	$map[$y - 1][$x + 1] = '#';
+	map_point($map[$y][$x + 1], '?', $void);
+	$map[$y + 1][$x + 1] = '#';
+}
+
+function	compile_map(&$regex, $step, $x, $y, &$map)
+{
+	$position = [$x, $y];
+	$branches = [];
+
+	display($map);
+	$void = "~";
+	echo "x=".$x.", y=".$y."\n\n";
+
+	while (isset($regex[$step])) {
+		switch ($regex[$step]) {
+			case "S":
+				move_south($x, $y, $map, $void);
+				break;
+			case "E":
+				move_east($x, $y, $map, $void);
+				break;
+			case "N":
+				move_north($x, $y, $map, $void);
+				break;
+			case "W":
+				move_west($x, $y, $map, $void);
+				break;
+
+			case "(":
+				return ['status' => 'branching', 'at' => ['step' => $step, 'x' => $x, 'y' => $y]];
+				break;
+			case ")":
+				$step++;
+				foreach ($branches as &$b)
+					$b['step'] = $step;
+				return ['status' => 'branched', 'branches' => $branches];
+				break;
+			case "|":
+				$branches[] = ['y' => $y, 'x' => $x];
+				list($x, $y) = $position;
+				break;
+		}
+		$step++;
+	}
+}
+
+function	extract_paths($regex, &$i)
+{
+	$paths[] = [];
+
+	for ( ; $i < count($regex); $i++) {
+		$step = $regex[$i];
+
+		if ($step == "(") {
+			$i++;
+			$opt = extract_paths($regex, $i);
+			foreach ($paths as $p) {
+				foreach ($opt as $o) {
+					$new_paths[] = $p + $o;
+				}
+			}
+			$paths = $new_paths;
+			print_R($paths);
+		}
+		else if ($step == ")") {
+			$options[] = $paths[0];
+			return $options;
+		}
+		else if ($step == "|") {
+			$options[] = $paths[0];
+			$path[] = [];
+		}
+		else {
+			foreach ($paths as &$p)
+				$p[] = $step;
+		}
+	}
+	return $paths;
+}
+
+function	solve($regex)
 {
 	$map = [
 		['#', '?', '#'],
 		['?', 'X', '?'],
 		['#', '?', '#'],
 	];
-	$pos[] = [
-		'y' => 1,
-		'x' => 1,
-	];
-	$y = 1;
-	$x = 1;
+	$regex = str_split($regex);
 
-	foreach (str_split($regex) as $step) {
-		if ($step == "^" || $step == "$")
-			continue;
-		display($map);
-		echo "x=".$x.", y=".$y."\n\n";
+	$i = 0;
+	$paths = extract_paths($regex, $i);
 
-		switch ($step) {
-			case "S":
-				$map[++$y][$x] = '-';
-				if (++$y >= count($map)) {
-					$map[] = array_fill(0, count($map[0]), " ");
-					$map[] = array_fill(0, count($map[0]), " ");
-				}
-				$map[$y]		[$x - 1]	= '?';
-				$map[$y]		[$x]		= '.';
-				$map[$y]		[$x + 1]	= '?';
-
-				$map[$y + 1]	[$x - 1]	= '#';
-				$map[$y + 1]	[$x]		= '?';
-				$map[$y + 1]	[$x + 1]	= '#';
-				break;
-			case "E":
-				$map[$y][++$x] = '|';
-				if (++$x >= count($map[$y])) {
-					for ($i = 0; $i < count($map); $i++) {
-						$map[$i][] = " ";
-						$map[$i][] = " ";
-					}
-				}
-				$map[$y - 1]	[$x] = '?';
-				$map[$y]		[$x] = '.';
-				$map[$y + 1]	[$x] = '?';
-
-				$map[$y - 1]	[$x + 1] = '#';
-				$map[$y]		[$x + 1] = '?';
-				$map[$y + 1]	[$x + 1] = '#';
-				break;
-			case "N":
-				$map[--$y][$x] = '-';
-				if (--$y < 0) {
-					$y = 1;
-					array_unshift($map, array_fill(0, count($map[$y]), " "));
-					array_unshift($map, array_fill(0, count($map[$y]), " "));
-				}
-				$map[$y]		[$x - 1]	= '?';
-				$map[$y]		[$x]		= '.';
-				$map[$y]		[$x + 1]	= '?';
-
-				$map[$y - 1]	[$x - 1]	= '#';
-				$map[$y - 1]	[$x]		= '?';
-				$map[$y - 1]	[$x + 1]	= '#';
-				break;
-			case "W":
-				$map[$y][--$x] = '|';
-				if (--$x < 0) {
-					$x = 1;
-					foreach ($map as &$row) {
-						array_unshift($row, " ");
-						array_unshift($row, " ");
-					}
-				}
-				$map[$y - 1]	[$x] = '?';
-				$map[$y]		[$x] = '.';
-				$map[$y + 1]	[$x] = '?';
-
-				$map[$y - 1]	[$x - 1] = '#';
-				$map[$y]		[$x - 1] = '?';
-				$map[$y + 1]	[$x - 1] = '#';
-				break;
-			case "(":
-				$pos[] = ['y' => $y, 'x' => $x];
-				break;
-			case ")"://TODO
-			case "|":
-				list($y, $x) = array_pop($pos);
-				break;
-		}
+	foreach ($paths as $p) {
+		echo implode($p);
 	}
+	die();
+	//compile_map($r, 0, 1, 1, $map);
+
 	foreach ($map as &$row) {
 		$row = array_map(function ($r) {
 			return ($r == '?') ? '#' : $r;
 		}, $row);
 	}
-	return $map;
-}
-
-function	solve($regex)
-{
-	$map = compile_map($regex);
 
 	return $map;
 }
