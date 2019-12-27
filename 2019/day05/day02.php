e5081122ade30ad6777c1e89022e575dece4a165
vagrant <vagrant@homestead>
2019-12-27 18:05:38 +0000

    updated day 5

--- 2019/day05/day02.php
+++ 2019/day05/day02.php
@@ -3,10 +3,7 @@ include_once __DIR__.'/IntcodeProcessor.php';
 
 $input = file_get_contents($argv[1]);
 
-// Part One.
 $program = new IntcodeProcessor(array_map('intval', explode(',', $input)));
 $program->initMemory();
 $output = $program->run();
 echo "\nResult: \e[0m\e[32m", array_pop($output),"\e[0m is the last output before halt.\n";
-
-// Part Two.
